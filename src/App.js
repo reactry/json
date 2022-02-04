b22be9cc04c97090b17ceed3ef17d7197911d3e3
Ankur Seth <iankurseth@gmail.com>
2022-02-05 01:31:00 +0530

    tailwind and cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function App () {
+	return (
+		<div className="App">
+			<main className="flex min-h-screen bg-blue-500 text-white">
+				<h2 className="text-[120px] m-auto">json</h2>
+			</main>
+		</div>
+	);
 }
 
 export default App;
