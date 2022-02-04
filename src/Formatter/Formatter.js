35f60086306e9d7904d4ac9accc41ad1a4169e29
Ankur Seth <iankurseth@gmail.com>
2022-02-05 04:10:56 +0530

    Added clickable Input and Output tab headings

--- src/Formatter/Formatter.js
+++ src/Formatter/Formatter.js
@@ -1,9 +1,12 @@
+import React from 'react';
 
 
 
 export default function Formatter ({
 	settings
 }) {
+
+	let [showOutput, setShowOutput] = React.useState(false);
 
 	let indentationItems = settings.indentations.map((v, i) => {
 		return (
@@ -13,14 +16,29 @@ export default function Formatter ({
 		);
 	});
 
+	let tabHeadingClass = "px-8 py-4";
+	let activeTabClass = tabHeadingClass + " cursor-pointer hover:bg-slate-300";
+	let passiveTabClass = tabHeadingClass + " bg-slate-100";
+
+	let tabs = null;
+	if (showOutput) {
+		tabs = <div className="grow text-lg flex">
+			<h1 className={activeTabClass} onClick={() => setShowOutput(!showOutput)}>Input</h1>
+			<h1 className={passiveTabClass}>Output</h1>
+		</div>
+	} else {
+		tabs = <div className="grow text-lg flex">
+			<h1 className={passiveTabClass}>Input</h1>
+			<h1 className={activeTabClass} onClick={() => setShowOutput(!showOutput)}>Output</h1>
+		</div>
+	}
+
 	return (
 		<div className="Formatter px-4 py-16 bg-slate-300">
 			<div className="max-w-4xl mx-auto shadow">
-				<div className="bg-slate-200 px-6 py-4 flex">
-					<div className="grow">
-						<h1 className="text-xl">Input</h1>
-					</div>
-					<div className="flex">
+				<div className="bg-slate-200 flex">
+					{tabs}
+					<div className="flex px-4 py-4">
 						{indentationItems}
 					</div>
 				</div>
