b5dae9550a3350c84400ead313e7cdec3b3dfd83
Ankur Seth <iankurseth@gmail.com>
2022-02-05 04:49:28 +0530

    Corrected mobile look

--- src/Formatter/Formatter.js
+++ src/Formatter/Formatter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Tabs from './Tabs';
 import Indentations from './Indentations';
+import Tabs from './Tabs';
 
 
 
@@ -22,9 +22,9 @@ export default function Formatter ({
 	return (
 		<div className="Formatter px-4 py-16 bg-slate-300">
 			<div className="max-w-4xl mx-auto shadow">
-				<div className="bg-slate-200 flex">
-					<Tabs {...tabsProps} />
+				<div className="bg-slate-200 md:flex">
 					<Indentations {...indentationsProps} />
+					<Tabs {...tabsProps} />
 				</div>
 				<div className="bg-slate-100 px-4 py-4">
 					<textarea name="" id="" cols="30" rows="10" className="w-full border-2 border-slate-500"></textarea>
